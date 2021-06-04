bbae2ed91fd3c03728074d583019887ef3a1ffd1
gido-pnnl <84922873+gido-pnnl@users.noreply.github.com>
2021-06-04 14:40:13 -0700

    Update bMNTD_nulls.R

--- bMNTD_nulls.R
+++ bMNTD_nulls.R
@@ -13,7 +13,7 @@ peak.data <- peak.data[-w,] #remove non-existent peaks from peak data
 peak.data[peak.data > 1] = 1 #turns peak data into present/absent
 
 tree_type <- "tree_type" #name of tree
-Sample_Name <- "sample_name" #name of dataset
+Sample_Name <- "sample_name" #name of data set
 
 if(!dir.exists(paste0(tree_type, "_Null_Results"))){ #creates directory for null bMNTD matrix storage
   dir.create(paste0(tree_type, "_Null_Results"))
@@ -22,12 +22,12 @@ if(!dir.exists(paste0(tree_type, "_Null_Results"))){ #creates directory for null
 ##bMNTD process
 
 matched <- match.phylo.data(tree, peak.data) #matches tree to peak data, stores as list with "data" and "phy"
-coph <- cophenetic(match$phy) #creates cophenetic distance matrix relating species
+coph <- cophenetic(matched$phy) #creates cophenetic distance matrix relating species
 
 range = 1:999 #number of iterations
 
 for(i in range){ #in this iteration, data is structured with peaks as rows and samples as columns - returns distances between samples
-  bMNTD.rand = as.matrix(comdistnt(t(match$data), taxaShuffle(coph), abundance.weighted = F, exclude.conspecifics = F)) #calculates bMNTD relating samples with randomized distance matrix
+  bMNTD.rand = as.matrix(comdistnt(t(matched$data), taxaShuffle(coph), abundance.weighted = F, exclude.conspecifics = F)) #calculates bMNTD relating samples with randomized distance matrix
   write.csv(bMNTD.rand, paste(tree_type, "_Null_Results/FTICR_", Sample_Name, "_", tree_type, "_bMNTD_rep", i, ".csv", sep = ""), quote = F) #writes matrix as .csv
   rm("bMNTD.rand")
   
